b1b78ca7c051a61116f6fffb9bcbe95ff652f2a4
DelMaradinho <admitriev91@gmail.com>
2021-05-26 00:24:26 +0300

    create: mobile navbar position fixed

--- js/main.js
+++ js/main.js
@@ -33,3 +33,8 @@ var hotelSlider = new Swiper('.hotel-slider', {
       },
 
   });
+
+  var menuButton = document.querySelector('.menu-button');
+  menuButton.addEventListener('click', function(){
+    document.querySelector('.navbar-bottom').classList.toggle('.navbar-bottom--visible');
+  });
